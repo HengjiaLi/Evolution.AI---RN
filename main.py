cf9c3616ffa160f3a6651b2eafabee78db7148a8
Hengjia's Mac <Ben@Hengjias-MacBook-Air>
2020-06-26 16:42:06 +0100

    add cuda

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ import random
 import numpy as np
 import csv
 
+os.environ["CUDA_CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
 import torch
 from torch.utils.tensorboard import SummaryWriter
 from torch.autograd import Variable
