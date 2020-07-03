c96e0eadaf26978b62910a99d916ab51a5a8d19a
Hengjia's Mac <Ben@Hengjias-MacBook-Air>
2020-07-03 09:00:12 +0100

    simplified model

--- model_full.py
+++ model_full.py
@@ -1,3 +1,4 @@
+# RN uses the paper's paramters
 import numpy as np
 import torch
 import torch.nn as nn
@@ -10,14 +11,14 @@ class ConvInputModel(nn.Module):
     def __init__(self):
         super(ConvInputModel, self).__init__()
         
-        self.conv1 = nn.Conv2d(3, 24, 3, stride=2, padding=1)
-        self.batchNorm1 = nn.BatchNorm2d(24)
-        self.conv2 = nn.Conv2d(24, 24, 3, stride=2, padding=1)
-        self.batchNorm2 = nn.BatchNorm2d(24)
-        self.conv3 = nn.Conv2d(24, 24, 3, stride=2, padding=1)
-        self.batchNorm3 = nn.BatchNorm2d(24)
-        self.conv4 = nn.Conv2d(24, 24, 3, stride=2, padding=1)
-        self.batchNorm4 = nn.BatchNorm2d(24)
+        self.conv1 = nn.Conv2d(3, 32, 3, stride=2, padding=1)
+        self.batchNorm1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, 3,stride=2, padding=1)
+        self.batchNorm2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, 3, stride=2, padding=1)
+        self.batchNorm3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 256, 3, stride=2, padding=1)
+        self.batchNorm4 = nn.BatchNorm2d(256)
 
         
     def forward(self, img):
@@ -41,14 +42,17 @@ class FCOutputModel(nn.Module):
     def __init__(self):
         super(FCOutputModel, self).__init__()
 
-        self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 10)
-
+        self.fc2 = nn.Linear(1000, 500)
+        self.fc3 = nn.Linear(500, 100)
+        self.fc4 = nn.Linear(100, 10)
     def forward(self, x):
         x = self.fc2(x)
         x = F.relu(x)
         x = F.dropout(x)
         x = self.fc3(x)
+        x = F.relu(x)
+        x = F.dropout(x)
+        x = self.fc4(x)
         return F.log_softmax(x, dim=1)
 
 class BasicModel(nn.Module):
@@ -89,16 +93,16 @@ class RN(BasicModel):
         
         if self.relation_type == 'ternary':
             ##(number of filters per object+coordinate of object)*3+question vector
-            self.g_fc1 = nn.Linear((24+2)*3+18, 256)
+            self.g_fc1 = nn.Linear((256+2)*3+18, 256)
         else:
             ##(number of filters per object+coordinate of object)*2+question vector
-            self.g_fc1 = nn.Linear((24+2)*2+18, 256)
+            self.g_fc1 = nn.Linear((256+2)*2+18, 2000)
 
-        self.g_fc2 = nn.Linear(256, 256)
-        self.g_fc3 = nn.Linear(256, 256)
-        self.g_fc4 = nn.Linear(256, 256)
+        self.g_fc2 = nn.Linear(2000, 2000)
+        self.g_fc3 = nn.Linear(2000, 2000)
+        self.g_fc4 = nn.Linear(2000, 2000)
 
-        self.f_fc1 = nn.Linear(256, 256)
+        self.f_fc1 = nn.Linear(2000, 1000)
 
         self.coord_oi = torch.FloatTensor(args.batch_size, 2)
         self.coord_oj = torch.FloatTensor(args.batch_size, 2)
@@ -128,18 +132,15 @@ class RN(BasicModel):
 
 
     def forward(self, img, qst):
-        x = self.conv(img) ## x = (64 x 24 x 5 x 5)
-        
+        x = self.conv(img) ## x = (64(batch_size) x 256 x 5 x 5) 
         """g"""
-        mb = x.size()[0]
-        n_channels = x.size()[1]
-        d = x.size()[2]
-        # x_flat = (64 x 25 x 24)
-        x_flat = x.view(mb,n_channels,d*d).permute(0,2,1)
+        mb = x.size()[0]#64
+        n_channels = x.size()[1]#256
+        d = x.size()[2]#5
+        x_flat = x.view(mb,n_channels,d*d).permute(0,2,1)#64 x 25 x 256
         
         # add coordinates
-        x_flat = torch.cat([x_flat, self.coord_tensor],2)
-        
+        x_flat = torch.cat([x_flat, self.coord_tensor],2)#64 x 25 x (256+2)
 
         if self.relation_type == 'ternary':
             # add question everywhere
@@ -169,22 +170,22 @@ class RN(BasicModel):
             x_ = x_full.view(mb * (d * d) * (d * d) * (d * d), 96)  # (64*25*25*25x3*26+18) = (1.000.000, 96)
         else:
             # add question everywhere
-            qst = torch.unsqueeze(qst, 1)
-            qst = qst.repeat(1, 25, 1)
-            qst = torch.unsqueeze(qst, 2)
+            qst = torch.unsqueeze(qst, 1)# (64x1x18)
+            qst = qst.repeat(1, 25, 1)# (64x25x18)
+            qst = torch.unsqueeze(qst, 2)# (64x25x1x18)
 
             # cast all pairs against each other
-            x_i = torch.unsqueeze(x_flat, 1)  # (64x1x25x26+18)
-            x_i = x_i.repeat(1, 25, 1, 1)  # (64x25x25x26+18)
-            x_j = torch.unsqueeze(x_flat, 2)  # (64x25x1x26+18)
-            x_j = torch.cat([x_j, qst], 3)
-            x_j = x_j.repeat(1, 1, 25, 1)  # (64x25x25x26+18)
+            x_i = torch.unsqueeze(x_flat, 1)  # (64x1x25x258+18)
+            x_i = x_i.repeat(1, 25, 1, 1)  # (64x25x25x258+18)
+            x_j = torch.unsqueeze(x_flat, 2)  # (64x25x1x258+18)
+            x_j = torch.cat([x_j, qst], 3)# (64x25x1x276)
+            x_j = x_j.repeat(1, 1, 25, 1)  # (64x25x25x276)
             
             # concatenate all together
-            x_full = torch.cat([x_i,x_j],3) # (64x25x25x2*26+18)
+            x_full = torch.cat([x_i,x_j],3) # (64x25x25x(276+258)=534)
         
             # reshape for passing through network
-            x_ = x_full.view(mb * (d * d) * (d * d), 70)  # (64*25*25x2*26*18) = (40.000, 70)
+            x_ = x_full.view(mb * (d * d) * (d * d), 534)  # (64x25x25,534)
             
         x_ = self.g_fc1(x_)
         x_ = F.relu(x_)
@@ -194,12 +195,11 @@ class RN(BasicModel):
         x_ = F.relu(x_)
         x_ = self.g_fc4(x_)
         x_ = F.relu(x_)
-        
         # reshape again and sum
         if self.relation_type == 'ternary':
-            x_g = x_.view(mb, (d * d) * (d * d) * (d * d), 256)
+            x_g = x_.view(mb, (d * d) * (d * d) * (d * d), 2000)
         else:
-            x_g = x_.view(mb, (d * d) * (d * d), 256)
+            x_g = x_.view(mb, (d * d) * (d * d), 2000)
 
         x_g = x_g.sum(1).squeeze()
         
@@ -215,7 +215,7 @@ class CNN_MLP(BasicModel):
         super(CNN_MLP, self).__init__(args, 'CNNMLP')
 
         self.conv  = ConvInputModel()
-        self.fc1   = nn.Linear(5*5*24 + 18, 256)  # question concatenated to all
+        self.fc1   = nn.Linear(5*5*24 + 18, 2000)  # question concatenated to all
         self.fcout = FCOutputModel()
 
         self.optimizer = optim.Adam(self.parameters(), lr=args.lr)
@@ -233,3 +233,4 @@ class CNN_MLP(BasicModel):
         x_ = F.relu(x_)
         
         return self.fcout(x_)
+
