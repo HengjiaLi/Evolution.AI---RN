df3b2007c8262144410074b939637888cc822377
Hengjia's Mac <Ben@Hengjias-MacBook-Air>
2020-07-04 07:55:59 +0100

    auto delete

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ parser.add_argument('--model', type=str, choices=['RN', 'CNN_MLP'], default='RN'
                     help='resume from model stored')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
-parser.add_argument('--epochs', type=int, default=20, metavar='N',
+parser.add_argument('--epochs', type=int, default=1, metavar='N',
                     help='number of epochs to train (default: 20)')
 parser.add_argument('--lr', type=float, default=0.0001, metavar='LR',
                     help='learning rate (default: 0.0001)')
@@ -287,3 +287,9 @@ with open(f'./{args.model}_{args.seed}_log.csv', 'w') as log_file:
         csv_writer.writerow([epoch, train_acc_ternary, train_acc_binary,
                          train_acc_unary, test_acc_ternary, test_acc_binary, test_acc_unary])
     model.save_model(epoch)
+
+data_dir = "./data/sort-of-clevr.pickle"
+if os.path.isfile(data_dir):
+    os.remove(data_dir)
+else:    ## Show an error ##
+    print("Error: %s file not found" % data_dir)
