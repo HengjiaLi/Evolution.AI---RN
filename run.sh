a42880254661b0456a7c2e9a4df27b76f23a4df5
Hengjia's Mac <Ben@Hengjias-MacBook-Air>
2020-09-22 14:17:13 +0100

    remove files

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python sort_of_clevr_generator.py
-
-python main.py --model=RN      --epochs=20      --relation-type=binary
-
-python main.py --model=RN      --epochs=20      --relation-type=ternary
-
-python main.py --model=CNN_MLP --epochs=100
